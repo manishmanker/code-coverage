87fbcbe7f5b2197aa34b1ef3ac107a31d33d34f5
Saurabh Juneja <saurabhjuneja82@gmail.com>
2015-07-19 09:32:26 +0530

    US-1234: Changed Some Code
    US-1234: Changed Some Code

--- src/main/java/com/mkyong/core/App.java
+++ src/main/java/com/mkyong/core/App.java
@@ -4,21 +4,21 @@ public class App {
 	public static void main(String[] args) {
  
 		System.out.println(getHelloWorld());
-		System.out.println("Hello");
-		System.out.println("Hellooooo");
+		System.out.println("Hello1");
+		System.out.println("Hellooooo1");
  
 	}
  
 	public static String getHelloWorld() {
-		System.out.println("Hello");
-		System.out.println("Hello");
+		System.out.println("Hello2");
+		System.out.println("Hello2");
 		return "Hello World";
  
 	}
  
 	public static String getHelloWorld2() {
-			System.out.println("Hello");
-		return "Hello World 2";
+			System.out.println("Hello3");
+		return "Hello World 23";
  
 	}
 }
